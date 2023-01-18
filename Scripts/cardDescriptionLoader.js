052aa95a9643ee94ec4ddfb1e0935b84a587eeb5
Alexandros Touramanis <32987136+AlexTour@users.noreply.github.com>
2023-01-18 16:08:34 +0100

    Create cardDescriptionLoader.js

---
+++ Scripts/cardDescriptionLoader.js
@@ -0,0 +1,15 @@
+
+document.getElementById('room01-title').textContent = "ERDGESCHOSS (1/2)";
+document.getElementById('room01-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+
+document.getElementById('room02-title').textContent = "ERDGESCHOSS (2/2)";
+document.getElementById('room02-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+
+document.getElementById('room03-title').textContent = "OBERGESCHOSS (1/3)";
+document.getElementById('room03-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+
+document.getElementById('room04-title').textContent = "OBERGESCHOSS (2/3)";
+document.getElementById('room04-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+
+document.getElementById('room05-title').textContent = "OBERGESCHOSS (3/3)";
+document.getElementById('room05-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
